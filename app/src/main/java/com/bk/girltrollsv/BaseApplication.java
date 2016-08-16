a9fdc23929e7f71642ba78a28b01ea6804135c06
tonquangtu <tonquangtu49@gmail.com>
2016-08-16 15:06:49 +0700

    init facebook sdk

--- app/src/main/java/com/bk/girltrollsv/BaseApplication.java
+++ app/src/main/java/com/bk/girltrollsv/BaseApplication.java
@@ -1,8 +1,16 @@
 package com.bk.girltrollsv;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
 
 import com.bk.girltrollsv.util.SharedPrefUtils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 /**
@@ -24,9 +32,31 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sharedPrefUtils = new SharedPrefUtils(getApplicationContext());
+
+//        printHashKey();
     }
 
     public static SharedPrefUtils getSharedPreferences() {
         return sharedPrefUtils;
+    }
+
+    public void printHashKey() {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.bk.girltrollsv",
+                    PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
--- app/src/main/java/com/bk/girltrollsv/ui/activity/SplashActivity.java
+++ app/src/main/java/com/bk/girltrollsv/ui/activity/SplashActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bk.girltrollsv.R;
+import com.facebook.FacebookSdk;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_splash);
     }
 }
